f3460cb9115c9b359155a18d009fa61c6a191010
liuqian62 <66772096+liuqian62@users.noreply.github.com>
2022-03-19 17:30:27 +0800

    read data and change to the useful format

--- CodeCraft-2022/src/CodeCraft-2022.cpp
+++ CodeCraft-2022/src/CodeCraft-2022.cpp
@@ -26,20 +26,22 @@ private:
     string _Output = "output/solution.txt";
 
     int qos_constraint;
-    vector<Site> sites; 
+    vector<Site> sites;
     vector<vector<int>> table;
     vector<vector<bool>> status;
+    vector<string> customer_ids;
+    vector<string> site_names;
 
 public:
-/**
- * @brief Construct a new Distributor object
- * 
- * @param Input_demand path to demand.csv
- * @param Input_bandwidth path to bandwidth.csv
- * @param Input_qos path to qos.csv
- * @param Input_config path to config.ini
- * @param Output path to save solution.txt
- */
+    /**
+     * @brief Construct a new Distributor object
+     *
+     * @param Input_demand path to demand.csv
+     * @param Input_bandwidth path to bandwidth.csv
+     * @param Input_qos path to qos.csv
+     * @param Input_config path to config.ini
+     * @param Output path to save solution.txt
+     */
     Distributor(string Input_demand,
                 string Input_bandwidth,
                 string Input_qos,
@@ -47,63 +49,63 @@ public:
                 string Output);
     /**
      * @brief read data
-     * 
+     *
      */
     void read_data();
 
     /**
      * @brief read config file
-     * 
+     *
      */
     void read_config();
 
     /**
      * @brief read site bandwidth
-     * 
+     *
      */
     void read_bandwidth();
 
     /**
      * @brief read qos
-     * 
+     *
      */
     void read_qos();
 
     /**
      * @brief read demand file
-     * 
+     *
      */
     void read_demand();
 
     /**
      * @brief include distribute, check and output
-     * 
+     *
      */
     void distribute();
 
     /**
      * @brief to distribute with less cost
-     * 
+     *
      */
     void do_distribute();
 
     /**
      * @brief check is there any customer site do not have enough bandwidth
      * if there is some customer sites do not have enough bandwidth, try to repair
-     * 
+     *
      */
     void final_check();
 
     /**
      * @brief output the result as result.txt to the right directory
-     * 
+     *
      */
     void output();
 
-/**
- * @brief Destroy the Distributor object
- * 
- */
+    /**
+     * @brief Destroy the Distributor object
+     *
+     */
     ~Distributor();
 };
 
@@ -127,80 +129,155 @@ void Distributor::read_data()
     read_demand();
 }
 
-void Distributor::read_config(){
-       ifstream ifs;
+void Distributor::read_config()
+{
+    ifstream ifs;
     ifs.open(_Input_config, ios::in);
 
-	if (!ifs.is_open())
-	{
-		cout << "failed to open "<<_Input_config << endl;
-		return;
-	}
+    if (!ifs.is_open())
+    {
+        cout << "failed to open " << _Input_config << endl;
+        return;
+    }
 
-    	string buf;
-        getline(ifs, buf);
-	while (getline(ifs, buf))
-	{
+    string buf;
+    getline(ifs, buf);
+    while (getline(ifs, buf))
+    {
 
         char *r = nullptr;
-        
-        r = strtok((char*)buf.data(),"=");
+
+        r = strtok((char *)buf.data(), "=");
         r = strtok(NULL, "=");
         this->qos_constraint = atoi(r);
 
-		cout << qos_constraint << endl;
-	}
+        cout << qos_constraint << endl;
+    }
     ifs.close();
-
 }
-    void Distributor::read_bandwidth(){
-           ifstream ifs;
+void Distributor::read_bandwidth()
+{
+    ifstream ifs;
     ifs.open(_Input_bandwidth, ios::in);
 
-	if (!ifs.is_open())
-	{
-		cout << "failed to open "<<_Input_bandwidth << endl;
-		return;
-	}
+    if (!ifs.is_open())
+    {
+        cout << "failed to open " << _Input_bandwidth << endl;
+        return;
+    }
 
-    	string buf;
-        getline(ifs, buf);
-	while (getline(ifs, buf))
-	{
+    string buf;
+    getline(ifs, buf);
+    while (getline(ifs, buf))
+    {
 
         char *r = nullptr;
-        
-        r = strtok((char*)buf.data(),",");
-        string site_name =r;
+
+        r = strtok((char *)buf.data(), ",");
+        string site_name = r;
         r = strtok(NULL, ",");
         int band = atoi(r);
-        Site site(site_name,band);
+        Site site(site_name, band);
         sites.push_back(site);
-		// cout << qos_constraint << endl;
-	}
-    for(int i=0;i<sites.size();i++){
-        cout<<sites[i]._name<<endl;
-        cout<<sites[i].available()<<endl;
+    }
+    for (int i = 0; i < sites.size(); i++)
+    {
+        cout << sites[i]._name << endl;
+        cout << sites[i].available() << endl;
     }
     ifs.close();
+}
+void Distributor::read_qos()
+{
+    ifstream ifs;
+    ifs.open(_Input_qos, ios::in);
 
+    if (!ifs.is_open())
+    {
+        cout << "failed to open " << _Input_qos << endl;
+        return;
     }
-    void Distributor::read_qos(){
 
-    }
-    void Distributor::read_demand(){
+    string buf;
+    char *r = nullptr;
+    getline(ifs, buf);
+    r = strtok((char *)buf.data(), ",");
 
+    r = strtok(NULL, ",");
+    while (r != NULL)
+    {
+        string customer_id = r;
+        customer_ids.push_back(customer_id);
+        r = strtok(NULL, ",");
     }
-    // void distribute();
-    void Distributor::do_distribute(){
 
-    }
-    void Distributor::final_check(){
+    while (getline(ifs, buf))
+    {
 
+        r = strtok((char *)buf.data(), ",");
+        string site_name = r;
+        site_names.push_back(site_name);
+        r = strtok(NULL, ",");
+        vector<int> rows;
+        vector<bool> statu;
+        while (r != NULL)
+        {
+            int qos = atoi(r);
+            if (qos < qos_constraint)
+            {
+                rows.push_back(sites[site_names.size()]._max_bandwidth);
+                statu.push_back(true);
+            }
+            else
+            {
+                rows.push_back(0);
+                statu.push_back(false);
+            }
+            r = strtok(NULL, ",");
+        }
+        table.push_back(rows);
+        status.push_back(statu);
     }
-    void Distributor::output(){
+    for (int i = 0; i < customer_ids.size(); i++)
+    {
+        cout << customer_ids[i] << endl;
+    }
 
+    for (int i = 0; i < site_names.size(); i++)
+    {
+        cout << site_names[i] << endl;
     }
+    for (int i = 0; i < table.size(); i++)
+    {
+        for (int j = 0; j < table[0].size(); j++)
+        {
+            cout << table[i][j] << ",";
+        }
+        cout << endl;
+    }
+    for (int i = 0; i < status.size(); i++)
+    {
+        for (int j = 0; j < status[0].size(); j++)
+        {
+            cout << status[i][j] << ",";
+        }
+        cout << endl;
+    }
+    ifs.close();
+}
+void Distributor::read_demand()
+{
+}
+// void distribute();
+void Distributor::do_distribute()
+{
+}
+void Distributor::final_check()
+{
+}
+void Distributor::output()
+{
+}
 void Distributor::distribute()
 {
     do_distribute();
